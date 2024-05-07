3477415927904230ee775904a10766a7595f393e
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2024-05-07 17:26:07 +0300

    Yedek

---
+++ Demo-OVTR/overtime/webapp/controller/Base.Controller.js
@@ -0,0 +1,113 @@
+sap.ui.define([
+	"sap/ui/base/ManagedObject"
+], function(
+	ManagedObject
+) {
+	"use strict";
+
+	return ManagedObject.extend("employee.overtime.controller.Base.Controller", {
+        onInit: function () {},
+        /**
+         * Convenience method for accessing the router.
+         * @public
+         * @returns {sap.ui.core.routing.Router} the router for this component
+         */
+        getRouter: function () {
+          return sap.ui.core.UIComponent.getRouterFor(this);
+        },
+  
+        /**
+         * Convenience method for getting the view model by name.
+         * @public
+         * @param {string} [sName] the model name
+         * @returns {sap.ui.model.Model} the model instance
+         */
+        getModel: function (sName) {
+          var oModel =  this.getView().getModel(sName) || this.getOwnerComponent().getModel(sName);
+
+          return oModel;
+        },
+  
+        /**
+         * Convenience method for setting the view model.
+         * @public
+         * @param {sap.ui.model.Model} oModel the model instance
+         * @param {string} sName the model name
+         * @returns {sap.ui.mvc.View} the view instance
+         */
+        setModel: function (oModel, sName) {
+          return this.getView().setModel(oModel, sName);
+        },
+  
+        /**
+         * Getter for the resource bundle.
+         * @public
+         * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+         */
+        getResourceBundle: function () {
+          return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+        },
+  
+        getText: function (sText, aParam = []) {
+          return this.getResourceBundle().getText(sText, aParam);
+        },
+  
+        openBusyFragment: function (sTextCode, aMessageParameters) {
+          var oDialog = this._getBusyFragment();
+          var that = this;
+          if (sTextCode) {
+            oDialog.setText(this.getText(sTextCode, aMessageParameters));
+          } else {
+            oDialog.setText(this.getText("pleaseWait", [] ));
+          }
+  
+          setTimeout(function () {
+            oDialog.open();
+          }, 100);
+        },
+  
+        closeBusyFragment: function () {
+          var oDialog = this._getBusyFragment();
+          var that = this;
+          var _close = function () {
+            oDialog.close();
+          };
+          setTimeout(_close, 500);
+        },
+  
+       /**
+       * Convenience method for calling 
+       * @public
+       * @returns {ConfirmationDialog} the router for this component
+       */
+        callConfirmDialog: function (
+          sTitle,
+          sDialogType,
+          sState,
+          sConfirmation,
+          oBeginButtonProp,
+          oEndButtonProp
+        ) {
+          
+        },
+       /**
+       * Convenience method for get generic Busy fragment
+       * @private
+       * @returns {com.taha.ux.annualleaveapprovement.ui.BusyDialog} the router for this component
+       */
+        _getBusyFragment: function () {
+            if (!this.oBusyDialog) {
+              this.oBusyDialog = sap.ui.xmlfragment(
+                "com.taha.ux.annualleaveapprovement.view.fragment.GenericBusyDialog",
+                this
+              );
+    
+              this.getView().addDependent(this.oBusyDialog);
+            } else {
+              this.oBusyDialog.close();
+            }
+    
+            return this.oBusyDialog;
+          },
+      });
+});
--- Demo-OVTR/overtime/webapp/controller/Main.controller.js
+++ Demo-OVTR/overtime/webapp/controller/Main.controller.js
@@ -9,7 +9,52 @@ sap.ui.define([
 
         return Controller.extend("employee.overtime.controller.Main", {
             onInit: function () {
+                var oLeaveRequestModel = new sap.ui.model.json.JSONModel({
+                    LeaveRequestList: []
+                });
+              
+                this.getOwnerComponent().setModel(oLeaveRequestModel, "LeaveRequestModel");
+                var oModel = this.getOwnerComponent().getModel();
+                oModel.read("/OverTimeEmployeeSet", {
+                    success: function (oData, oResponse) {
+                      var index = 1;
+                      oData.results.forEach((element) => {
+                        element["id"] = index;
+                        index++;
+                        // console.log(oModel)
+                        // console.log(oData)
+                      });
+                      
+                      oLeaveRequestModel.setProperty("/LeaveRequestList", oData.results);
+                    //   var ApprovalStatus = oApproveModel.getProperty(
+                    //     "/ApprovalSet/0/ApprovelStatus"
+                    //   );
+                    //   if (ApprovalStatus == null || ApprovalStatus == "") {
+                    //     oApproveModel.setProperty(
+                    //       "/ApprovalSet/0/ApprovelStatus",
+                    //       "Onay Bekleniyor"
+                    //     );
+                    //   }
+                      var a = 0;
+                    },
+                    error: function (oError) {
+                      console.log(oError);
+                    },
+                  }); 
 
-            }
+            },
+            
+              onTableItemPress: function (oEvent) {
+                console.log("a");
+
+                var oViewModel =  this.getView().getModel("LeaveRequestModel"); 
+                var oTable = this.getView().byId("mainTable");
+                var sPath = oEvent.getParameter("listItem").getBindingContext("LeaveRequestModel").getPath();
+                var selectedRow= oTable.getModel("LeaveRequestModel").getProperty(sPath);
+                var oModel = this.getModel();
+                var a = 1;
+              },
+
+        
         });
     });
